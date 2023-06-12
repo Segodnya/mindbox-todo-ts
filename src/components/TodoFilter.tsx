feca92c9bf89e356454082bc6679ada2f6a4e1bc
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-12 21:54:33 +0300

    Добавил фильтрацию по статусу задач

---
+++ src/components/TodoFilter.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ToggleButton, ToggleButtonGroup } from 'react-bootstrap';
+
+interface Props {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const TodoFilter: React.FC<Props> = ({ value, onChange }) => {
+  return (
+    <ToggleButtonGroup type="radio" name="options" value={value} onChange={(value) => onChange(value)}>
+      <ToggleButton variant="outline-primary" value="all" id="all">
+        All
+      </ToggleButton>
+      <ToggleButton variant="outline-primary" value="completed" id="completed">
+        Completed
+      </ToggleButton>
+      <ToggleButton variant="outline-primary" value="uncompleted" id="uncompleted">
+        Uncompleted
+      </ToggleButton>
+    </ToggleButtonGroup>
+  );
+};
+
+export default TodoFilter;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { ListGroup } from 'react-bootstrap';
 import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
+import TodoFilter from './TodoFilter';
 
 interface Todo {
   id: number;
@@ -12,6 +13,7 @@ interface Todo {
 const TodoList: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [newTodo, setNewTodo] = useState<string>('');
+  const [filter, setFilter] = useState<string>('all');
 
   const handleAddTodo = () => {
     if (newTodo.trim() === '') return;
@@ -31,25 +33,23 @@ const TodoList: React.FC = () => {
 
   const completedTodos = todos.filter((todo) => todo.completed);
   const uncompletedTodos = todos.filter((todo) => !todo.completed);
+  const filteredTodos = filter === 'completed' ? completedTodos : filter === 'uncompleted' ? uncompletedTodos : todos;
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm newTodo={newTodo} setNewTodo={setNewTodo} handleAddTodo={handleAddTodo} />
       <hr />
-      <h2>Uncompleted Todos</h2>
+      <TodoFilter value={filter} onChange={(value) => setFilter(value)} />
+      <hr />
+      <h2>{filter === 'completed' ? 'Completed' : filter === 'uncompleted' ? 'Uncompleted' : 'All'} Todos</h2>
       <ListGroup>
-        {uncompletedTodos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} handleToggleTodo={handleToggleTodo} />
         ))}
       </ListGroup>
       <hr />
-      <h2>Completed Todos</h2>
-      <ListGroup>
-        {completedTodos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} handleToggleTodo={handleToggleTodo} />
-        ))}
-      </ListGroup>
+      <p>Number of uncompleted tasks: {uncompletedTodos.length}</p>
     </div>
   );
 };
